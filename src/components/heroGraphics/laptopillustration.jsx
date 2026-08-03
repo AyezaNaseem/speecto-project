528dde8321af3cc74d45ab7b14c1f8190d241d2c
Ayeza Naseem <ayezanaseem4@gmail.com>
2026-08-04 00:01:28 +0500

    Upload complete Speecto project

--- src/components/heroGraphics/laptopillustration.jsx
+++ src/components/heroGraphics/laptopillustration.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./laptopillustration.css";
 import roundChartsFixed from "../../asset/psRound1_fixed.webp"; 
+import roundChartsBack from "../../asset/psRound2.webp";
 import manImg from "../../asset/man.webp";
 import stepsBase from "../../asset/steps.webp";
+import stepsBottom from "../../asset/stepsBottom.webp"; // New Bottom Glow/Shadow Layer
 
 export default function LaptopIllustration() {
   return (
     <div className="laptop-graphic">
+      <img src={stepsBottom} alt="" className="lg-steps-bottom" aria-hidden="true" />
+      <img src={roundChartsBack} alt="" className="lg-charts-back float-back" aria-hidden="true" />
       <img src={stepsBase} alt="" className="lg-steps" aria-hidden="true" />
-       <img src={manImg} alt="Web development team illustration" className="lg-man lg-man--enter" />
+      <img src={manImg} alt="Web development team illustration" className="lg-man lg-man--enter" />
       <img src={roundChartsFixed} alt="" className="lg-charts float-medium" aria-hidden="true" />
     </div>
   );

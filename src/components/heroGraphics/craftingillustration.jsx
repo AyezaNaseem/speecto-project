8969ac15424d4c2a4eef2d587243a3396bf06146
Ayeza Naseem <ayezanaseem4@gmail.com>
2026-08-04 23:32:23 +0500

    Fix laptop illustration hover swap and orbit animation issue

--- src/components/heroGraphics/craftingillustration.jsx
+++ src/components/heroGraphics/craftingillustration.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './craftingillustration.css';
 import reactIcon from "../../asset/reactIcon2.webp";
 import img1 from "../../asset/bannerReactCircle1.webp";
@@ -6,7 +6,14 @@ import img2 from "../../asset/bannerReactCircle2.webp";
 import img3 from "../../asset/bannerReactCircle3.webp";
 import img4 from "../../asset/bannerReactCircle4.webp";
 
+import LaptopIllustration from "./laptopillustration";
+import MobileIllustration from "./mobileillustration";
+import LaptopIllustration2 from "./laptopillustration2";
+import TestingIllustration from "./testingillustration";
+
 export default function CraftingIllustration() {
+  const [activeGraphic, setActiveGraphic] = useState(null);
+
   return (
     <div className="ci-container">
       <div className="ci-orbit-path-static">
@@ -50,21 +57,51 @@ export default function CraftingIllustration() {
         <div className="sub-orbit s8-2"><span className="inner-dot"></span></div>
         <div className="ti-orbit-item item-8"></div>
         
-        <div className="ti-orbit-item img-node moving-node mn-1">
-          <img src={img1} alt="node-img" className="node-vector-img" />
+        {/* Circle 1 (Img 1 - LaptopIllustration2) */}
+        <div 
+          className="ti-orbit-item img-node moving-node mn-1"
+          onMouseEnter={() => setActiveGraphic('laptop2')}
+          onMouseLeave={() => setActiveGraphic(null)}
+        >
+          <img src={img1} alt="laptopillustration-2-icon" className="node-vector-img" />
         </div>
-        <div className="ti-orbit-item img-node moving-node mn-2">
-          <img src={img2} alt="node-img" className="node-vector-img" />
+
+        {/* Circle 2 (Img 2 - MobileIllustration) */}
+        <div 
+          className="ti-orbit-item img-node moving-node mn-2"
+          onMouseEnter={() => setActiveGraphic('mobile')}
+          onMouseLeave={() => setActiveGraphic(null)}
+        >
+          <img src={img2} alt="mobile-illustration-icon" className="node-vector-img" />
         </div>
-        <div className="ti-orbit-item img-node moving-node mn-3">
-          <img src={img3} alt="node-img" className="node-vector-img" />
+
+        {/* Circle 3 (Img 3 - LaptopIllustration1) */}
+        <div 
+          className="ti-orbit-item img-node moving-node mn-3"
+          onMouseEnter={() => setActiveGraphic('laptop1')}
+          onMouseLeave={() => setActiveGraphic(null)}
+        >
+          <img src={img3} alt="laptopillustration-1-icon" className="node-vector-img" />
         </div>
-        <div className="ti-orbit-item img-node moving-node mn-4">
-          <img src={img4} alt="node-img" className="node-vector-img" />
+
+        {/* Circle 4 (Img 4 - TestingIllustration) */}
+        <div 
+          className="ti-orbit-item img-node moving-node mn-4"
+          onMouseEnter={() => setActiveGraphic('testing')}
+          onMouseLeave={() => setActiveGraphic(null)}
+        >
+          <img src={img4} alt="testing-illustration-icon" className="node-vector-img" />
         </div>
 
       </div>
-      <img src={reactIcon} alt="reactIcon" className="ci-react-icon"/>
+
+      <div className="ci-center-stage">
+        {(!activeGraphic || activeGraphic === 'default') && <img src={reactIcon} alt="reactIcon" className="ci-react-icon"/>}
+        {activeGraphic === 'laptop1' && <div className="ci-center-graphic-component"><LaptopIllustration /></div>}
+        {activeGraphic === 'mobile' && <div className="ci-center-graphic-component"><MobileIllustration /></div>}
+        {activeGraphic === 'laptop2' && <div className="ci-center-graphic-component"><LaptopIllustration2 /></div>}
+        {activeGraphic === 'testing' && <div className="ci-center-graphic-component"><TestingIllustration /></div>}
+      </div>
     </div>
   );
 }
